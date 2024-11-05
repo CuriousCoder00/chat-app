675c026359eb01852cd2e3b545a6a0ba2d9416b9
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-05 16:57:11 +0530

    feat(client): update homepage and header with new title and link

--- apps/client/components/Header/header.tsx
+++ apps/client/components/Header/header.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "../theme-toggler";
+import Link from "next/link";
 
 type Props = {};
 export const Header = ({}: Props) => {
   return (
     <header className="flex items-center justify-between p-2 px-8">
       <div className=" flex items-center space-x-4">
-        <div className="text-2xl font-bold">Chat Verse</div>
+        <Link href={"/"} className="text-2xl font-bold">
+          Chat Verse
+        </Link>
       </div>
       <div className="flex items-center justify-center gap-3">
         <ModeToggle />
