351dbd2fdfb36bb16c34865cc841261f99418d71
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-06 03:33:18 +0530

    feat(client): refactor authentication setup with NextAuth and add credential provider

--- apps/client/auth.ts
+++ apps/client/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import db from "@/lib/prisma-config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getUserByID } from "./lib/utils/user.utils";
+
+import authConfig from "./lib/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -38,5 +39,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
-  providers: [Google],
+  ...authConfig,
 });
--- apps/client/middleware.ts
+++ apps/client/middleware.ts
@@ -1,1 +1,35 @@
-export { auth as middleware } from "@/auth"
+import NextAuth from "next-auth";
+import authConfig from "./lib/auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith("/api/auth");
+  const isPublicRoute = ["/"].includes(nextUrl.pathname);
+  const isAuthRoute = ["/auth/login", "/auth/register"].includes(
+    nextUrl.pathname
+  );
+
+  const DEFAULT_LOGIN_REDIRECT = "/chats";
+
+  if (isApiAuthRoute) return;
+  if (isAuthRoute && isLoggedIn) {
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  }
+  if (isAuthRoute && !isLoggedIn) return;
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
