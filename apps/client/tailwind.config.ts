4d4fce1f4ce4f7a1cc1337d95dc38c465a25905a
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-05 15:06:45 +0530

    feat(client): implement theme toggler and update styles

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -48,7 +48,7 @@ const config: Config = {
   				'2': 'hsl(var(--chart-2))',
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
+  				'5': 'hsl(var(--chart-5))',
   			}
   		},
   		borderRadius: {
