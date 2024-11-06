792c213ff8830bb1dc8006ae1886d8ea00c7c32f
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-06 12:54:43 +0530

    feat(client): add username existence check in updateUsername action; enhance UserAvatar component with profile navigation

--- apps/client/actions/user.actions.ts
+++ apps/client/actions/user.actions.ts
@@ -5,6 +5,17 @@ import { getUserByEmail } from "@/lib/utils/user.utils";
 export const updateUsername = async (username: string, email: string) => {
   const user = await getUserByEmail(email);
   try {
+    const usernameExists = await db.user.findFirst({
+      where: {
+        username,
+      },
+    });
+    if (usernameExists) {
+      return {
+        success: false,
+        message: "Username already exists",
+      };
+    }
     await db.user.update({
       where: { id: user?.id },
       data: {
--- apps/client/components/user-avatar.tsx
+++ apps/client/components/user-avatar.tsx
@@ -8,26 +8,38 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { LogOut } from "lucide-react";
+import { LogOut, UserPenIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   image: string;
   name: string;
 };
 export const UserAvatar = ({ image, name }: Props) => {
+  const router = useRouter();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Avatar>
+        <Avatar className="cursor-pointer">
           <AvatarImage src={image} />
           <AvatarFallback>{name?.[0]}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="mt-2">
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Button
+            onClick={() => router.push("/profile")}
+            variant={"default"}
+            className="w-full flex justify-start items-center bg-transparent text-black dark:text-white cursor-pointer"
+          >
+            <UserPenIcon />
+            <span>Profile</span>
+          </Button>
+        </DropdownMenuItem>
         <DropdownMenuItem asChild>
           <Button
             variant={"default"}
