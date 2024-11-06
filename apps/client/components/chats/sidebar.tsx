da60dbc7da2801f3e2b6acd193515c4a338f0448
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-06 07:25:40 +0530

    feat(client): enhance chat sidebar items with last message preview and transition effects

--- apps/client/components/chats/sidebar.tsx
+++ apps/client/components/chats/sidebar.tsx
@@ -4,12 +4,14 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 type ChatSidebarProps = {
   children: React.ReactNode;
 };
+
 type ChatSidebarItemsProps = {
   users: {
     name: string;
     image: string;
   }[];
 };
+
 export const ChatSidebar = ({ children }: ChatSidebarProps) => {
   return (
     <div className="flex flex-col justify-start items-start border w-96 h-full overflow-y-auto px-2 gap-2">
@@ -25,7 +27,7 @@ export const ChatSidebarItems = ({ users }: ChatSidebarItemsProps) => {
         <Link
           key={index}
           href={"/chats/" + user.name}
-          className="flex items-center justify-start gap-3 w-full text-md hover:bg-gray-200 hover:text-gray-800 hover:dark:bg-gray-800 dark:text-gray-200 rounded-md p-2"
+          className="flex items-center justify-start gap-3 w-full text-md hover:bg-gray-200 hover:text-gray-800 hover:dark:bg-gray-800 dark:text-gray-200 rounded-md p-2 transition-colors duration-200 ease-in-out"
         >
           <Avatar>
             <AvatarImage
@@ -35,7 +37,16 @@ export const ChatSidebarItems = ({ users }: ChatSidebarItemsProps) => {
               alt={user.name}
             />
           </Avatar>
-          {user.name}
+          <div className="flex items-start justify-center flex-col w-full overflow-hidden">
+            {user.name}
+            <div className="flex justify-start flex-nowrap items-center w-full">
+              <span className="w-full text-ellipsis text-nowrap text-sm">
+                this is the last message data that was sent by either you or the
+                other user
+              </span>
+              ...
+            </div>
+          </div>
         </Link>
       ))}
     </div>
